980a5c7ae90d9eb239a9036ad38d2d153c567c74
Jane Kostov <kostovjane23@gmail.com>
2025-10-30 15:30:09 +0100

    Last commit

--- components/EventCard.tsx
+++ components/EventCard.tsx
@@ -22,7 +22,7 @@ const EventCard = ({ title, image, slug, location, date, time }: Props) => {
 
             <p className="title">{title}</p>
 
-            <div classNgame="datetime">
+            <div className="datetime">
                 <div>
                     <Image src="/icons/calendar.svg" alt="date" width={14} height={14} />
                     <p>{date}</p>
